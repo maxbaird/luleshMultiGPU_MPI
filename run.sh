70e62bfd6b0886cd1becb586cb40e78dbfaf78f0
Max Baird <mmb1@hw.ac.uk>
2019-03-08 17:06:04 +0000

    Updated run file

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #! /usr/bin/bash
 #srun --mpi=pmi2 -n 9 --gres=gpu --partition=amd-shortq ./lulesh.out 18
-srun --mpi=pmi2 -n 9 --gres=gpu --partition=amd-shortq ./lulesh.out 189
+#srun --mpi=pmi2 -n 9 --gres=gpu --partition=amd-shortq ./lulesh.out 189
+srun --mpi=pmi2 -n 8 --gres=gpu --partition=amd-shortq ./lulesh.out 192
