d62059e98da7de118de7952460313c55b9d5da64
Max Baird <mmb1@hw.ac.uk>
2019-03-12 22:58:32 +0000

    Modified run script to accept argument for number of elements to be passed to lulesh

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #! /usr/bin/bash
 #srun --mpi=pmi2 --nodes=1 --ntasks=7 --gres=gpu --partition=amd-shortq ./lulesh.out 200
 #srun --mpi=pmi2 -n 9 --gres=gpu --partition=amd-shortq ./lulesh.out 153
-srun --mpi=pmi2 -n 8 --gres=gpu --partition=amd-shortq ./lulesh.out 192
+#srun --mpi=pmi2 -n 8 --gres=gpu --partition=amd-shortq ./lulesh.out 192
+srun --mpi=pmi2 -n 8 --gres=gpu --partition=amd-shortq ./lulesh.out $1
