1c7ed5959df8e62bbb6b180a578ebec65f2bac67
Max Baird <mmb1@hw.ac.uk>
2019-03-16 14:03:55 +0000

    Added comment

--- lulesh.cu
+++ lulesh.cu
@@ -2241,6 +2241,7 @@ void CalcVolumeForceForElems(const Real_t hgcoef,Domain *domain)
       bool hourg_gt_zero = hgcoef > Real_t(0.0);
       if (hourg_gt_zero || domain->myRank == 0)
       {
+        //TODO change back quantum to 0.001
         FTI_Protect_Kernel(&domain->snapshotCount, domain->myRank, 1, 0.001,(CalcVolumeForceForElems_kernel<true>), dimGrid,block_size,0,domain->streams[1],
           domain->volo.raw()+offset, 
           domain->v.raw()+offset, 
