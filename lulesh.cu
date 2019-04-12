835f18ce2137dbe75c78fd2531f94160b660e241
Max Baird <mmb1@hw.ac.uk>
2019-04-12 16:24:07 +0100

    Disabled snapshots

--- lulesh.cu
+++ lulesh.cu
@@ -4211,8 +4211,8 @@ int main(int argc, char *argv[])
 
   ProtectVariables(locDom, nx, &its);
 
-  int res = 0;
-  size_t snapshotCount = 0;
+  //int res = 0;
+  //size_t snapshotCount = 0;
 
   //res = FTI_Snapshot();
 
@@ -4222,13 +4222,13 @@ int main(int argc, char *argv[])
     // Allow MPI_Allreduce to overlap with computation on GPU
     //TimeIncrement(locDom) ;
 
-    res = FTI_Snapshot();
+    //res = FTI_Snapshot();
 
-    if(res == FTI_DONE){
-      snapshotCount++;
-      fprintf(stdout, "%d: FTI: Checkpointing successful:%d\n", myRank, its);
-      fflush(stdout);
-    }
+    //if(res == FTI_DONE){
+    //  snapshotCount++;
+    //  fprintf(stdout, "%d: FTI: Checkpointing successful:%d\n", myRank, its);
+    //  fflush(stdout);
+    //}
 
     LagrangeLeapFrog(locDom) ;
 
@@ -4253,7 +4253,7 @@ int main(int argc, char *argv[])
     printf("Elapsed Time=%8.4e\n",elapsed_time);
 	  printf("Problem size=%ix%ix%i\n",    nx,nx,nx);
 	  printf("Iteration count=%i\n",    its);	
-    printf("Snapshots= %zu\n", snapshotCount);
+    //printf("Snapshots= %zu\n", snapshotCount);
 
     Real_t e_zero;
     cudaMemcpy(&e_zero, locDom->e.raw(), sizeof(Real_t),cudaMemcpyDeviceToHost) ;
